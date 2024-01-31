5c966a50391606ffd9607d1c9350b6eb468db8df
Gus Esquivel <gesquive@gmail.com>
2024-01-30 22:35:15 -0600

    stop checking host key

--- cci-clone.sh
+++ cci-clone.sh
@@ -10,8 +10,10 @@ git --version
 
 # Add github as an ssh known host
 mkdir -p ${HOME}/.ssh
-if ! grep github.com ${HOME}/.ssh/known_hosts > /dev/null; then
-     echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >> ${HOME}/.ssh/known_hosts
+if ! grep github.com ${HOME}/.ssh/config > /dev/null; then
+     echo "Host github.com" >> ${HOME}/.ssh/config
+     echo "  StrictHostKeyChecking no" >> ${HOME}/.ssh/config
+     echo "  UserKnownHostsFile /dev/null" >> ${HOME}/.ssh/config
 fi
 # hide the paragraph about being in a detached head state, no one cares
 git config advice.detachedHead false
